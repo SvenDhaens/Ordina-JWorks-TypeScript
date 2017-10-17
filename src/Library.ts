e1a4e9415ede9825646e59d5452bc249b043211b
SvenDhaens <svendhaens@gmail.com>
2017-10-17 16:06:16 +0200

    Done deal

--- src/Library.ts
+++ src/Library.ts
@@ -2,7 +2,7 @@ class Library {
 
     items:Array<Item>;
 
-    constructor(public books: Array<Book>,public movies:Array<Movie>){
+    constructor(private books: Array<Book>,private movies:Array<Movie>){
 
     }
 
@@ -12,8 +12,21 @@ class Library {
 
         return new Library(books,movies);
     }
+
     getAll(): Array<Item> {
         this.items = (<Item[]>this.books).concat(this.movies);
         return this.items;
+    }
+
+    addMovie(){
+        let movie = new Movie("random movie","random genre","random description",99,"random");
+        this.movies.push(movie);
+        movie.render(document.getElementById("items"));
+    }
+
+    addBook(){
+        let book = new Book("random Book",new Author("random author"),"random genre","random description");
+        this.books.push(book);
+        book.render(document.getElementById("items"));
     }
 }
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 
 let jsonParser = new JSONParser();
-
+let library:Library
 jsonParser.getJSON("items.json", data => {
-    let library:Library = Library.fromJSON(data);
+     library = Library.fromJSON(data);
 
     let itemContainer: HTMLElement = document.getElementById("items");
     library.getAll().forEach(item => item.render(itemContainer));
 
 });
+
+function addMovietoLib(){
+    library.addMovie();
+}
+
+function addBooktoLib(){
+    library.addBook();
+}
